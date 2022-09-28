ab295235ffad352a806fef80cbc2a270f38d9371
j33p1n <vtdag1@gmail.com>
2022-09-28 12:39:08 -0400

    Remove tailwindColors dependency

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import LogoLockup from './LogoLockup';
-import { getTailwindColors } from '../../utilities/getTailwindConfig';
 
 export interface LogoProps {
 	className?: string;
@@ -9,13 +8,11 @@ export interface LogoProps {
 	white?: boolean;
 }
 
-const colors = getTailwindColors();
-
 const IconLogo = (props: LogoProps) => {
 	return (
 		<svg className={classNames(props.className && props.className)} width="35" height="33" viewBox="0 0 35 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path fillRule="evenodd" clipRule="evenodd" d="M7.564 17.82H5.153V14.981L15.825 8.716V0L0 9.086V32.984H15.825V27.068H7.564V17.82Z" fill={props.white ? colors.white : colors.orange['300']} />
-			<path fillRule="evenodd" clipRule="evenodd" d="M19.104 0V8.716L29.775 14.981V17.82H27.362V27.068H19.104V32.984H34.926V9.086L19.104 0Z" fill={props.white ? colors.white : colors.orange['300']} />
+			<path fillRule="evenodd" clipRule="evenodd" d="M7.564 17.82H5.153V14.981L15.825 8.716V0L0 9.086V32.984H15.825V27.068H7.564V17.82Z" fill={props.white ? '#FFFFFF' : '#EA701B'} />
+			<path fillRule="evenodd" clipRule="evenodd" d="M19.104 0V8.716L29.775 14.981V17.82H27.362V27.068H19.104V32.984H34.926V9.086L19.104 0Z" fill={props.white ? '#FFFFFF' : '#EA701B'} />
 		</svg>
 	);
 };
